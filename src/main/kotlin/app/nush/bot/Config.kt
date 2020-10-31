03d3c765a19c887c9f2e2ee396cbc3355cdc2005
Lam Jun Rong <junron1@outlook.com>
2020-10-31 15:13:06 +0800

    Switch to vault
    Fixes #9

--- src/main/kotlin/app/nush/bot/Config.kt
+++ src/main/kotlin/app/nush/bot/Config.kt
@@ -1,28 +1,61 @@
 package app.nush.bot
+import com.bettercloud.vault.Vault
+import com.bettercloud.vault.VaultConfig
+import com.fasterxml.jackson.databind.ObjectMapper
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import java.io.File
 
 @Serializable
 data class Config(
-    val discordToken: String,
-    val projectsCategoryId: String,
+    val discordToken: String = "",
+    val projectsCategoryId: String = "",
     val botPrefix: String = "!",
-    val guildId: String,
-    val memberRole: String,
-    val excoRole: String,
+    val guildId: String = "",
+    val memberRole: String = "",
+    val excoRole: String = "",
     val dev: Boolean = true,
-    val alumniRole: String,
-    val guestRole: String,
-    val excoChannelId: String,
-    val githubToken: String
+    val alumniRole: String = "",
+    val guestRole: String = "",
+    val excoChannelId: String = "",
+    val githubToken: String = ""
 ) {
     companion object {
+        val config: Config by lazy {
+            val configFile = File("config.json")
+            val config = if (configFile.exists()) {
+                Json.parse(serializer(), configFile.readText())
+            } else {
+                val token = getToken()
+                val vault = Vault(
+                    VaultConfig()
+                        .engineVersion(2)
+                        .address("https://vault.nush.app")
+                        .token(token)
+                        .build()
+                )
+                val secrets = vault
+                    .logical()
+                    .read("apps/appventure-bot").data
+                ObjectMapper().convertValue(secrets, Config::class.java)
+            }
+            config.copy(botPrefix = if (config.dev) "dev" + config.botPrefix else config.botPrefix)
+        }
 
-        private val _config =
-            Json.parse(serializer(), File("config.json").readText())
-        val config =
-            _config.copy(botPrefix = if (_config.dev) "dev" + _config.botPrefix else _config.botPrefix)
+        private fun getToken(): String {
+            val vault = Vault(
+                VaultConfig()
+                    .engineVersion(2)
+                    .address("https://vault.nush.app")
+                    .build()
+            )
+            val password = System.getenv("VAULT_PASSWORD")
+            val response = vault.auth().loginByUserPass(
+                "appventure-bot",
+                password
+            )
+            return response.authClientToken
+        }
 
     }
 }
