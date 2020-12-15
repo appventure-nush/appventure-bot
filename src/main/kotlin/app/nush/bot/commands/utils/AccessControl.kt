27d970a3c55a889da22bc79151e371927a77c438
Lam Jun Rong <junron1@outlook.com>
2020-12-15 21:17:46 +0800

    Fix bug in isExco

--- src/main/kotlin/app/nush/bot/commands/utils/AccessControl.kt
+++ src/main/kotlin/app/nush/bot/commands/utils/AccessControl.kt
@@ -6,5 +6,5 @@ import com.jessecorbett.diskord.dsl.Bot
 suspend fun isExco(discordId: String, guildId: String, bot: Bot): Boolean {
     val roles =
         bot.clientStore.guilds[guildId].getMember(discordId).roleIds
-    return config.excoRole !in roles && !config.dev
+    return config.excoRole in roles || config.dev
 }
